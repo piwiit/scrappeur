66eb0cea4abe7393518f60111f40a4ce9c6f8bcc
piwiit <bouillaux@hotmail.fr>
2020-10-09 01:55:45 +0200

    fin exo crypto

--- drack_trader/spec/scrappeur_spec.rb
+++ drack_trader/spec/scrappeur_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../lib/scrappeur'
 
-# describe 'Crypto Scrap should return a global array' do
-#   it 'test the global resul' do
-#     expect(final_result).to be_kind_of(Array)
-#   end
-#   it 'should return a hash with the name of crypto and the value' do
-#     expect(final_result).not_to be_nil
-#   end
-# end
+describe 'Crypto Scrap should return a global array' do
+  it 'test the global resul' do
+    expect(final_result).to be_kind_of(Array)
+  end
+  it 'should return a hash with the name of crypto and the value' do
+    expect(final_result).not_to be_nil
+  end
+end
 
 describe 'Crypto Scrap should scrap data from a website' do
   it "should return a value BTC.. else the program don't work. Long life for BTC" do
-    expect(final_result).to include('{"STRAT"=>0.413126}')
+    expect(crypto_name).to include('BTC')
   end
 end
