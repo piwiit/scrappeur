1223aa9555a9f26fcfa095d6cd96e65a9adb6af1
piwiit <bouillaux@hotmail.fr>
2020-10-09 02:02:42 +0200

    fin exo crypto

--- drack_trader/lib/scrappeur.rb
+++ drack_trader/lib/scrappeur.rb
@@ -14,6 +14,7 @@ end
 @result = []
 
 def crypto_name
+  # recherche du nom de la valeur souhaiter nomé ici symbol : placé ensuite dans un tableau
   name = open_url.xpath('//td[contains(@class,"symbol")]')
   name.each do |v|
     @array_name << v.text
@@ -22,18 +23,23 @@ def crypto_name
 end
 
 def crypto_value
+  # recherche des prix des noms demandé plus haut : placezr ensuite dans un tableau
   value = open_url.xpath('//td[contains(@class,"price")]')
   value.each do |price|
+    # insertion du resultat dans un tableau et suppression des "$" et ","
     @array_price << price.text.delete('$,').to_f
   end
   @array_price
 end
 
 def final_result
+  # Associer les 2 tableaux entre eux
   final_array = crypto_name.zip(crypto_value)
+  # creation d'un hash par Noms / Valeurs pour tout les elment du tableau
   final_array.each do |n, v|
     my_hash = {}
     my_hash[n] = v
+    # Insertion des dit element dans un dernier tableau final
     @result << my_hash
   end
   puts @result
